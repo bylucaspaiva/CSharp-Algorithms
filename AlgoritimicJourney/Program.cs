55dc77fef345d5d2c318b2b7acab1e03c47816d5
Lucas Paiva <lucasp4iva@gmail.com>
2022-12-26 22:05:50 -0300

    Feat: add method that returns the maximum value

--- AlgoritimicJourney/Program.cs
+++ AlgoritimicJourney/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Algorithms {
+    class Program
+    {
+        static int findMaximum(int a, int b, int c)
+        {
+            // this is the most efficient way of test which variable is the max value
+            int max = a;
+            if (b > max)
+            {
+                max = b;
+            }
+            if (c > max)
+            {
+                c = max;
+            }
+
+            return max;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(findMaximum(8, 2, 4));
+        }
+    }
+}
+
