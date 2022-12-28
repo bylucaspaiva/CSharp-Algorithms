dd8dab757cfb6db5ad2935959309122b1a67f955
Lucas Paiva <lucasp4iva@gmail.com>
2022-12-27 23:10:11 -0300

    Feat: calls fing method to test architecture of folders

--- AlgoritimicJourney/Program.cs
+++ AlgoritimicJourney/Program.cs
@@ -1,28 +1,16 @@
 ﻿using System;
+using AlgoritimicJourney.SimpleAlgorithms;
 
 namespace Algorithms {
     class Program
     {
-        static int findMaximum(int a, int b, int c)
-        {
-            // this is the most efficient way of test which variable is the max value
-            int max = a;
-            if (b > max)
-            {
-                max = b;
-            }
-            if (c > max)
-            {
-                c = max;
-            }
-
-            return max;
-        }
-
         static void Main(string[] args)
         {
-            Console.WriteLine(findMaximum(8, 2, 4));
+            int result = SimpleAlgorithms.findMaximum(8, 4, 5);
+            Console.WriteLine(result); 
         }
+
+      
     }
 }
 
---
+++ AlgoritimicJourney/SimpleAlgorithms/SimpleAlgorithms.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoritimicJourney.SimpleAlgorithms
+{
+   public static class SimpleAlgorithms
+    {
+        public static int findMaximum(int a, int b, int c)
+        {
+            // this is the most efficient way of test which variable is the max value
+            int max = a;
+            if (b > max)
+            {
+                max = b;
+            }
+            if (c > max)
+            {
+                c = max;
+            }
+
+            return max;
+        }
+
+    }
+}
