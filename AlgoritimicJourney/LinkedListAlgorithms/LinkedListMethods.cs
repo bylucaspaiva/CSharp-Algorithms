ae58c8bab338152d444f5c4b84dd85d8b9aaaec4
Lucas Paiva <lucasp4iva@gmail.com>
2023-01-02 18:24:58 -0300

    Feat: trying common operations

--- AlgoritimicJourney/LinkedListAlgorithms/LinkedListMethods.cs
+++ AlgoritimicJourney/LinkedListAlgorithms/LinkedListMethods.cs
@@ -8,6 +8,20 @@ namespace AlgoritimicJourney.LinkedListAlgorithms
 {
     static public class LinkedListMethods
     {
+        public static void LinkedListOperations()
+        {
+            LinkedList<string> listy = new LinkedList<string>();
+            //AddLast, AddFirst
+            listy.AddLast("Lucas");
+            listy.AddLast("Kessia");
+            listy.AddLast("Arthur");
 
+            foreach(string item in listy)
+            {
+                Console.Write(item + "->");
+            }
+            Console.Write("null");
+
+        }
     }
 }
--- AlgoritimicJourney/Program.cs
+++ AlgoritimicJourney/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using AlgoritimicJourney.ArrayAlgorithms;
+using AlgoritimicJourney.LinkedListAlgorithms;
 using AlgoritimicJourney.SimpleAlgorithms;
 using AlgoritimicJourney.StringAlgorithms;
 
@@ -8,10 +9,9 @@ namespace Algorithms {
     {
         static void Main(string[] args)
         {
-            int[] arr1 = {1, 2, 3, 4, 9, 756, 5, 10};
-            int[] arr2 = { -4, 5, -6, 8, 22, 36 };
-            ArrayMethods.RotateArray(arr1, 2);
-            Array.ForEach(arr1, Console.WriteLine);
+
+            LinkedListMethods.LinkedListOperations();
+            
         }
 
       
