9e6cf9d7e93e7da7493cb05c5f42e30ecc126417
Lucas Paiva <lucasp4iva@gmail.com>
2023-01-03 19:48:00 -0300

    Feat: add DictionaryOperations method to hashMethods class

--- AlgoritimicJourney/HashBasedStructures/HashBasedMethods.cs
+++ AlgoritimicJourney/HashBasedStructures/HashBasedMethods.cs
@@ -6,19 +6,45 @@ using System.Threading.Tasks;
 
 namespace AlgoritimicJourney.HashBasedStructures
 {
-    internal class HashBasedMethods
+    public class HashBasedMethods
     {
+        public void DictionaryOperations()
+        {
+            var employee = new Employee("Lucas", 1, "ti");
+            var employee2 = new Employee("Valdirene", 2, "enfermagem");
+            var employee3 = new Employee("Marcelo", 3, "construção civil");
+
+            var employeesById = new Dictionary<int, Employee>();
+
+            Employee person;
+
+            employeesById.Add(employee.id, employee);
+            employeesById.Add(employee2.id, employee2);
+            employeesById.Add(employee3.id, employee3);
+
+
+            if (employeesById.TryGetValue(2, out person))
+            {
+                Console.WriteLine(person.name + " : " + person.department);
+            }
+        }
+
+        public void DisplayContent()
+        {
+            
+        }
     }
 
     public class Employee
     {
-        string name;
-        int id;
-        string department;
+        public string name;
+        public int id;
+        public string department;
         public Employee(string name, int id, string department) {
         this.name = name;
         this.id = id;
         this.department = department;
         }
+       
     }
 }
--- AlgoritimicJourney/Program.cs
+++ AlgoritimicJourney/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using AlgoritimicJourney.ArrayAlgorithms;
+using AlgoritimicJourney.HashBasedStructures;
 using AlgoritimicJourney.LinkedListAlgorithms;
 using AlgoritimicJourney.QueueAndStackAlgorithms;
 using AlgoritimicJourney.SimpleAlgorithms;
@@ -10,9 +11,14 @@ namespace Algorithms {
     {
         static void Main(string[] args)
         {
-            var queue = new QueueAndStackCommonMethods();
+            //var queue = new QueueAndStackCommonMethods();
 
-            queue.DisplayContent();
+            //queue.DisplayContent();
+
+            var hashMethods = new HashBasedMethods();
+
+            hashMethods.DictionaryOperations();
+
 
 
         }
