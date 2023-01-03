bb5e9cc4a46c0fa16cbafd34e7f964a36a78329a
Lucas Paiva <lucasp4iva@gmail.com>
2023-01-03 14:58:22 -0300

    Feat: add PrintBinary method;

--- AlgoritimicJourney/QueueAndStackAlgorithms/QueueAndStackMethods.cs
+++ AlgoritimicJourney/QueueAndStackAlgorithms/QueueAndStackMethods.cs
@@ -10,14 +10,41 @@ namespace AlgoritimicJourney.QueueAndStackAlgorithms
     {
         public void DisplayContent()
         {
+            PrintBinary(100);
+        }
+
+        public void SimpleQueue()
+        {
             var queue = new Queue<int>();
             queue.Enqueue(1);
             queue.Enqueue(2);
             queue.Enqueue(3);
 
             int removedItem = queue.Dequeue();
+            Console.WriteLine(queue.Peek());
             Console.WriteLine(removedItem);
 
+            int current;
+            while (queue.TryDequeue(out current))
+            {
+                Console.WriteLine(current);
+            }
+        }
+
+        public void PrintBinary(int n)
+        {
+            if (n < 0) return;
+            var queue = new Queue<int>();
+            queue.Enqueue(1);
+            for(int i = 0; i < n; i++)
+            {
+                int current = queue.Dequeue();
+                Console.WriteLine(current);
+                queue.Enqueue(current * 10);
+                queue.Enqueue(current * 10 + 1);
+            }
+
+            Console.WriteLine();    
         }
     }
 }
