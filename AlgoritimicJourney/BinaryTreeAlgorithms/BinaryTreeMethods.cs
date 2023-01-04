e9be19618280578e5745b62c2195d16edb6cb1ae
Lucas Paiva <lucasp4iva@gmail.com>
2023-01-04 12:13:08 -0300

    Feat: add BinarySearchTree method

--- AlgoritimicJourney/BinaryTreeAlgorithms/BinaryTreeMethods.cs
+++ AlgoritimicJourney/BinaryTreeAlgorithms/BinaryTreeMethods.cs
@@ -6,7 +6,35 @@ using System.Threading.Tasks;
 
 namespace AlgoritimicJourney.BinaryTreeAlgorithms
 {
-    internal class BinaryTreeMethods
+    public class BinaryTreeMethods
     {
+        class Node
+        {
+            public Node Left { get; set; }
+            public Node Right { get; set; }
+            public int Data { get; set; }
+        }
+
+        class BinarySearchTree
+        {
+            public static Node Insert(Node root, int value)
+            {
+                if(root == null)
+                {
+                    root = new Node();
+                    root.Data = value;
+                }else
+                {
+                    if(value < root.Data)
+                    {
+                        Insert(root.Left, value);
+                    }else if (value > root.Data)
+                    {
+                        Insert(root.Right, value);
+                    }
+                }
+                return root;
+            }
+        }
     }
 }
